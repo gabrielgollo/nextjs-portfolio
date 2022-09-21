19795b06716a1657f8a587b5df88dadd2f72d182
gabrielgollo <gabrielsantosgollo@gmail.com>
2022-09-21 14:04:16 -0300

    fix: navbar in mobile resolution

--- Components/NavigationBar/index.jsx
+++ Components/NavigationBar/index.jsx
@@ -1,8 +1,8 @@
-import { Link } from "@chakra-ui/react";
-
-import { Box, Center, Grid, GridItem } from "@chakra-ui/layout";
+import { Link, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import NextLink from "next/link"
+import { Box, Center, Container, Flex, Grid, GridItem, Stack } from "@chakra-ui/layout";
 import { IconButton } from "@chakra-ui/button";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { SunIcon, MoonIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 import { Icon } from "@iconify/react";
 
@@ -18,45 +18,74 @@ export const NavigationBar = () => {
       w="100%"
       zIndex={1}
     >
-      <Center>
-        <Grid templateColumns="2fr 1fr 1fr 2fr 1fr" gap={8}>
+      <Container
+        display="flex"
+        maxW="container.sm"
+        p={2}
+        wrap="wrap"
+        align="center"
+        justify="space-around"
+      >
+        <Flex align="center" mr={5} style={{ fontWeight:500}}>
+            <Link href="/" display="inline-flex" alignItems="center">
+              <MoonIcon />
+               Gabriel Gollo
+            </Link>
+        </Flex>
+
+        <Stack
+          direction={{ base: "column", sm: "row" }}
+          display={{ base: "none", sm: "flex" }}
+          width={{ base: "full", sm: "auto" }}
+          alignItems="center"
+          flexGrow={2}
+          mt={{ base: 4, sm: 0 }}
+          justify="space-around"
+          fontWeight={500}
+        >
+          <Link href="/" display="inline-flex" >Home</Link>
+          <Link href="/contact" display="inline-flex" >Contact</Link>
+          {/* <Link href="/about" display={"inline-flex"}>About</Link> */}
+          <Link href="https://github.com/gabrielgollo" display="inline-flex" alignItems="center" isExternal>
+            <Icon icon="akar-icons:github-fill" />
+             GitHub
+          </Link>
+        </Stack>
+
+        <Box flex={1} align="right">
+          <IconButton width="40px" onClick={toggleColorMode}><SunIcon /></IconButton>
+
           
-          <Grid gap={8}>
-            <Center>
-              <Link href="/" display={"inline-flex"}>
-                <MoonIcon />
-                Gabriel Gollo
-              </Link>
-            </Center>
-          </Grid>
+        
 
-          <Grid>
-            <Center><Link href="/" display={"inline-flex"} >Home</Link></Center>
-          </Grid>
-
-          <Grid>
-            <Center><Link href="/contact" display={"inline-flex"} >Contact</Link></Center>
-          </Grid>
-          
-          {/* <Grid>
-            <Center><Link href="/about" display={"inline-flex"}>About</Link></Center>
-          </Grid> */}
-
-          <Grid>
-            <Center >
-              <Link href="https://github.com/gabrielgollo" display={"inline-flex"}>
-                <Icon icon="akar-icons:github-fill" />
-                GitHub
-              </Link>
-            </Center>
-          </Grid>
-
-          <Grid>
-            <Center><IconButton width="40px" onClick={toggleColorMode}><SunIcon /></IconButton></Center>
-          </Grid>
-          
-        </Grid>
-      </Center>
+        <Box ml={2} display={{ base: "inline-block", sm: "none" }}>
+            <Menu isLazy={true} id="navbar-menu">
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList fontWeight={500}>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>Home</MenuItem>
+                </NextLink>
+                <NextLink href="/contact" passHref>
+                  <MenuItem as={Link}>Contact</MenuItem>
+                </NextLink>
+                <MenuItem
+                  as={Link}
+                  href="https://github.com/gabrielgollo"
+                  display="inline-flex"
+                >
+                  <Icon icon="akar-icons:github-fill" />
+                   Github
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container> 
     </Box>
   );
 };
